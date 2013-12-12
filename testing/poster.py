e1000fe43c70cbf7372c92aa5873a3dad8175828
Ryan Bressler <ryanbressler@gmail.com>
2013-12-12 12:46:12 -0700

    Update poster.py to work from stdin if only one argument provided.

--- testing/poster.py
+++ testing/poster.py
@@ -1,3 +1,39 @@
+#!/usr/bin/env python
+# Copyright (c) 2013, Ryan Bressler and other Contributors 
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of The Institute for Systems Biology, GraphSpectrometer nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
+Post contents of file/stdin to an endpoint as a form value in the "results" field.
+This may work for very large files, for that modify to post chunk by chunk or do 
+multipart file upload style posting.
+
+Example usage from stdin:
+cat file | poster.py http://endpoint
+
+From file:
+poster.py file http://endpoint
+'''
 import sys
 import json
 import urllib
@@ -5,8 +41,12 @@ import urllib2
 
 
 def main():
-	fo = open(sys.argv[1])
-	targetUrl = sys.argv[2]
+	fo = sys.stdin
+	targetUrl = sys.argv[1]
+
+	if len(sys.argv) > 2 :
+		fo = open(sys.argv[1])
+		targetUrl = sys.argv[2]
 	
 	data = urllib.urlencode({"results":fo.read()})
 	urllib2.urlopen(targetUrl,data)
